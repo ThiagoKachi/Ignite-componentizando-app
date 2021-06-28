45c70d778b69bd28db482dd54d6308196e4eb500
Thiago Kachinsky <tjk091@gmail.com>
2021-06-28 18:51:38 -0300

    Componentização do app

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+import { MovieCard } from './MovieCard';
+
 export function Content() {
-  // Complete aqui
+  const { movies, selectedGenre } = useContext(AppContext)
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie: any) => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  )
 }
